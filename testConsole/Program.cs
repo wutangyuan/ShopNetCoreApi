bc2dfaaa0e9241a3eb3672c919243f0f4236fa24
wutangyuan <wutangyuan1@gmail.com>
2020-10-24 20:36:41 +0800

    add new project

---
+++ testConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int m = 1, n = 0;
+            n = (m--) + (++m);//这N为什么等于4？
+            Console.WriteLine(n);
+
+        }
+    }
+}
