127efc3ca41c6ece83e8a946ea20716b8870fa54
wutangyuan <wutangyuan1@gmail.com>
2021-04-07 22:17:12 +0800

    测试使用redis连接和stringget、stringset的方法

--- testConsole/Program.cs
+++ testConsole/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using StackExchange.Redis;
 
 namespace testConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static  void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
@@ -12,6 +13,18 @@ namespace testConsole
             n = (m--) + (++m);//这N为什么等于4？
             Console.WriteLine(n);
 
+
+            //测试redis
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1");
+            var db = redis.GetDatabase(0);
+
+            var result = db.StringGet("name");
+            Console.WriteLine(result);
+
+           var isSet= db.StringSet("name1", "tangyuanname1");
+
+            Console.WriteLine(isSet);
         }
     }
 }
